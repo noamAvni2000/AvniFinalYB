6d7247656527e728fcd9da8bf58f45c7628cbcae
Alon <laviv.alon@gmail.com>
2025-09-21 16:17:12 +0300

    intial commit, drew the xml, and connected it to the main.

---
+++ app/src/main/java/com/example/avnifinalyb/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.avnifinalyb;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvEnterDetails, tvWelcome;
+    EditText etPassword;
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tvEnterDetails=findViewById(R.id.tvEnterDetails);
+        etPassword=findViewById(R.id.etPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        tvWelcome=findViewById(R.id.tvWelcome);
+
+    }
+}
