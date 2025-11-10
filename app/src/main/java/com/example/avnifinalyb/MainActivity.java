bf94ac4a961c7f1a372f433fd185f4c905fbc2fb
noamAvni2000 <nmnby7399@gmail.com>
2025-11-10 19:19:02 +0200

    added the second button first screen almost done, problems with project need help

--- app/src/main/java/com/example/avnifinalyb/MainActivity.java
+++ app/src/main/java/com/example/avnifinalyb/MainActivity.java
@@ -68,5 +68,14 @@ public class MainActivity extends AppCompatActivity {
                 }//a toast to show the user their login attempt failed
             }
         });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivitySignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
