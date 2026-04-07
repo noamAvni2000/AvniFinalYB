428d09cd7e50440b760f91c9a8a2e69e4073c7b3
noamAvni2000 <nmnby7399@gmail.com>
2026-04-07 17:22:35 +0300

    removed the latitude and longitude

--- app/src/main/java/com/example/avnifinalyb/MyItem.java
+++ app/src/main/java/com/example/avnifinalyb/MyItem.java
@@ -8,18 +8,14 @@ public class MyItem {
     private String landLocked;
     private String hasNoam;
     private String population;
-    private double latitude;
-    private double longitude;
 
-    public MyItem(String country, String continent, String religion, String landLocked, String hasNoam, String population, double latitude, double longitude) {
+    public MyItem(String country, String continent, String religion, String landLocked, String hasNoam, String population) {
         this.country = country;
         this.continent = continent;
         this.religion = religion;
         this.landLocked = landLocked;
         this.hasNoam = hasNoam;
         this.population = population;
-        this.latitude = latitude;
-        this.longitude = longitude;
     }
 
     public String getCountry() { return country; }
@@ -28,8 +24,6 @@ public class MyItem {
     public String getLandLocked() { return landLocked; }
     public String getHasNoam() { return hasNoam; }
     public String getPopulation() { return population; }
-    public double getLatitude() { return latitude; }
-    public double getLongitude() { return longitude; }
 }
 
 
